1be53d46fa65533e98e671249c27f718b08ad17a
Dave Compton <davecompton7@gmail.com>
2016-02-03 22:58:11 +0000

    moved main into Main.elm; switched to using StartApp;  update is not getting called yet.

--- Ball.elm
+++ Ball.elm
@@ -1,9 +1,11 @@
+module Ball where
+
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Time exposing (..)
 import Random exposing (..)
-import Html exposing (Html)
+import Html exposing (Html, fromElement)
 
 dt : Float
 dt = 0.1
@@ -23,20 +25,18 @@ init = { level = 0
        , seed = initialSeed 34342
        }
 
-view : Model -> Element
-view model = 
+view : Signal.Address Action -> Model -> Html
+view _ model = 
     let position = (scale * toFloat (model.shift), -scale * toFloat (model.level))
-    in collage 700 500
-         [ circle 8 |> filled blue |> move position ]
+    in collage 700 500 [ circle 8 |> filled blue |> move position ]
+       |> fromElement
 
-update : Model -> Model
-update model = let 
+
+type Action = Step
+
+update : Action -> Model -> Model
+update _ model = let 
                    deltaShift = map (\b -> if b then 1 else -1) bool
                    (delta, seed) = generate deltaShift model.seed
                in { level=(model.level)+1, shift = model.shift+delta, seed=seed}
-
-main : Signal Element
-main =
-  Signal.foldp (\_ model -> update model) init (every (dt * second))
-    |> Signal.map view
 
---
+++ Main.elm
@@ -0,0 +1,9 @@
+import Ball exposing (init, update, view)
+import StartApp.Simple exposing (start)
+
+main =
+  start
+    { model = init
+    , update = update
+    , view = view
+    }
