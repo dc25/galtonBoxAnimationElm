dabcab0c2487bd8db2c87782ed34b1b4cd103193
Dave Compton <davecompton7@gmail.com>
2016-02-05 14:49:31 +0000

    introducing GaltonModel and GModel

--- Ball.elm
+++ Ball.elm
@@ -10,30 +10,32 @@ import Config
 
 type Action = Step
 
-type alias Model = 
+type alias GaltonModel = 
   { level : Int
   , shift : Int
   , seed : Seed
   }
 
-init : Time -> Model 
+init : Time -> Model
 init t = 
-  { level = 0
+  GModel { level = 0
   , shift = 0
   , seed = initialSeed (truncate t)
   }
 
+type Model = GModel GaltonModel
+
 viewAsForm : Model -> Form
-viewAsForm model = 
+viewAsForm (GModel model) = 
   let dropLevel = toFloat (Config.height//2 - Config.headRoom)
       position = (Config.scale * toFloat (model.shift), dropLevel-Config.scale * toFloat (model.level))
   in circle 3 |> filled blue |> move position 
 
 update : Action -> Model -> (Model, Effects Action)
-update _ model = 
+update _ (GModel model) = 
   let deltaShift = map (\b -> if b then 1 else -1) bool
       (delta, seed) = generate deltaShift model.seed
-  in ( { model | 
+  in ( GModel { model | 
          level=(model.level)+1
        , shift = model.shift+delta
        , seed=seed}
